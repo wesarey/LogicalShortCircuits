d08b6e2082d91206938a6f5831236797ba65214a
Wes <wesarey@gmail.com>
2024-03-11 19:55:51 -0400

    Verbiage/formatting

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@
 
             // Be aware of short-circuiting within logical expressions
 
-            // Logical AND operations will short-circuit when the first false value is found, it no longer needs to evaluate any further
+            // Logical AND operations will short-circuit when the first false value is found, there is no need to evaluate any further
             if(skyIsBlue.IsTrue && grassIsGreener.IsTrue && thirdTruth.IsTrue) {     
                 // The only way we get here is if...
                 Console.WriteLine("All truths are true");
@@ -24,7 +24,14 @@
             {
                 // thirdTruth is null and would have thrown an error accessing the "IsTrue" property
                 Console.WriteLine("At least one truth is false");
+
+                // Because we made it inside this code block with no errors, we may think it is safe to access the properties of "thirdTruth"
+                // uncomment the line below and run to see results...
+
+                // Console.WriteLine(thirdTruth.Description);
             }
+
+
 
             // Logical OR operations will short-circuit when the first true value is found
             if (skyIsBlue.IsTrue || grassIsGreener.IsTrue || thirdTruth.IsTrue)
